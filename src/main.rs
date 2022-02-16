61f9b4c8a1600eddce24a52ce73abcccebb3ca67
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-02-16 12:40:50 -0300

    Changes to the test file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
+use beryllium::*;
+use ogl33::*;
+use std::mem::*;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = SDL::init(InitFlags::Everything).expect("Error starting SDL");
+    sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core)
+        .unwrap();
+    #[cfg(target_os = "macos")]
+    {
+        sdl.gl_set_attribute(SdlGlAttr::Flags, ContextFlag::ForwardCompatible)
+            .unwrap();
+    }
+
+    let _win = sdl
+        .create_gl_window(
+            "Hello window",
+            WindowPosition::Centered,
+            800,
+            600,
+            WindowFlags::Shown,
+        )
+        .expect("Error making the window");
+
+    'main_loop: loop {
+        while let Some(event) = sdl.poll_events().and_then(Result::ok) {
+            match event {
+                Event::Quit(_) => break 'main_loop,
+                _ => (),
+            }
+        }
+    }
+
+    unsafe {
+        load_gl_with(|f_name| _win.get_proc_address(f_name));
+        glClearColor(0.2, 0.2, 0.2, 1.0);
+
+        let mut vao = 0;
+        glGenVertexArrays(1, &mut vao);
+        assert_ne!(vao, 0);
+
+        let mut vbo = 0;
+        glGenBuffers(1, &mut vbo);
+        assert_ne!(vbo, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    }
+
+    type Vertex = [f32; 3];
+    const VERTICES: [Vertex; 3] = [[-0.5, -0.5, 0.0], [0.5, -0.5, 0.0], [0.0, 0.5, 0.0]];
+
+    const VERT_SHADER: &str = r#"#version 330 core
+        layout (location = 0) in vec3 pos;
+        void main() {
+            gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+        }
+    "#;
+
+    unsafe {
+        glBufferData(
+            GL_ARRAY_BUFFER,
+            size_of_val(&VERTICES) as isize,
+            VERTICES.as_ptr().cast(),
+            GL_STATIC_DRAW,
+        );
+        glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            size_of::<Vertex>().try_into().unwrap(),
+            0 as *const _,
+        );
+
+        let vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        assert_ne!(vertex_shader, 0);
+
+        glShaderSource(
+            vertex_shader,
+            1,
+            &(VERT_SHADER.as_bytes().as_ptr().cast()),
+            &(VERT_SHADER.len().try_into().unwrap()),
+        );
+
+        glCompileShader(vertex_shader);
+    }
 }
