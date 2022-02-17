9e458a9dbb8347e329281a04a9031001f593d766
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-02-17 15:51:15 -0300

    Abstracted most of the functionalities

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use beryllium::*;
 use ogl33::*;
 
 use crate::core::buffers;
+use crate::core::materials;
 use crate::core::utils::{
     types,
     init,
+    gl,
 };
 
 
@@ -19,20 +21,6 @@ const VERTICES: [types::Vertex; 3] =
         [0.0, 0.5, 0.0],
     ];
 
-const VERT_SHADER: &str = r#"#version 330 core
-    layout (location = 0) in vec3 pos;
-    void main() {
-        gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-    }
-"#;
-
-const FRAG_SHADER: &str = r#"#version 330 core
-    out vec4 final_color;
-    void main() {
-        final_color = vec4(1.0, 0.5, 0.2, 1.0);
-    }
-"#;
-
 
 fn main() {
     let (sdl, win) = init::set_up_window(WINDOW_TITLE);
@@ -42,89 +30,20 @@ fn main() {
     let vbo = buffers::vbo::new(&VERTICES);
     vao.link_vbo(vbo, 0);
     
-    unsafe {
-        glClearColor(0.2, 0.3, 0.3, 1.0);
-            
-        let vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-        assert_ne!(vertex_shader, 0);
-        glShaderSource(
-            vertex_shader,
-            1,
-            &(VERT_SHADER.as_bytes().as_ptr().cast()),
-            &(VERT_SHADER.len().try_into().unwrap()),
-        );
-        glCompileShader(vertex_shader);
-        let mut success = 0;
-        glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &mut success);
-        if success == 0 {
-            let mut v: Vec<u8> = Vec::with_capacity(1024);
-            let mut log_len = 0_i32;
-            glGetShaderInfoLog(
-                vertex_shader,
-                1024,
-                &mut log_len,
-                v.as_mut_ptr().cast(),
-            );
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Vertex Compile Error: {}", String::from_utf8_lossy(&v));
-        }
-
-        let fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-        assert_ne!(fragment_shader, 0);
-        glShaderSource(
-            fragment_shader,
-            1,
-            &(FRAG_SHADER.as_bytes().as_ptr().cast()),
-            &(FRAG_SHADER.len().try_into().unwrap()),
-        );
-        glCompileShader(fragment_shader);
-        let mut success = 0;
-        glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &mut success);
-        if success == 0 {
-            let mut v: Vec<u8> = Vec::with_capacity(1024);
-            let mut log_len = 0_i32;
-            glGetShaderInfoLog(
-                fragment_shader,
-                1024,
-                &mut log_len,
-                v.as_mut_ptr().cast(),
-            );
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Fragment Compile Error: {}", String::from_utf8_lossy(&v));
-        }
-
-        let shader_program = glCreateProgram();
-        assert_ne!(shader_program, 0);
-        glAttachShader(shader_program, vertex_shader);
-        glAttachShader(shader_program, fragment_shader);
-        glLinkProgram(shader_program);
-        let mut success = 0;
-        glGetProgramiv(shader_program, GL_LINK_STATUS, &mut success);
-        if success == 0 {
-            let mut v: Vec<u8> = Vec::with_capacity(1024);
-            let mut log_len = 0_i32;
-            glGetProgramInfoLog(
-                shader_program,
-                1024,
-                &mut log_len,
-                v.as_mut_ptr().cast(),
-            );
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Program Link Error: {}", String::from_utf8_lossy(&v));
-        }
-        glDeleteShader(vertex_shader);
-        glDeleteShader(fragment_shader);
-
-        glUseProgram(shader_program);
-    }
+    gl::set_clear_color(0.0, 0.0, 0.0, 1.0);
+    let shader = materials::shader::new(
+        "src/resources/shaders/shader1/test.vert",
+        "src/resources/shaders/shader1/test.frag",
+    );
+    shader.use_program();
 
     'main_loop: loop {
         // handle events this frame
         while let Some(event) = sdl.poll_events().and_then(Result::ok) {
-        match event {
-            Event::Quit(_) => break 'main_loop,
-            _ => (),
-        }
+            match event {
+                Event::Quit(_) => break 'main_loop,
+                _ => (),
+            }
         }
         // now the events are clear.
 
