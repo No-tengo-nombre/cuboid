0280af9efff1c447e6ec9a4c66471175602b5278
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-02-17 22:54:58 -0300

    Implemented the EBO

--- src/core/buffers/ebo.rs
+++ src/core/buffers/ebo.rs
@@ -1,4 +1,50 @@
-struct EBO {
-    id: u128,
-    count: u128,
+use gl;
+use gl::types::*;
+use std::mem::{
+    size_of,
+};
+
+
+pub struct EBO {
+    _id: u32,
+}
+
+
+pub fn new<T>(indices: &[T]) -> EBO {
+    let mut ebo = 0;
+    unsafe {
+        // Generate the VBO
+        gl::GenBuffers(1, &mut ebo);
+        assert_ne!(ebo, 0);
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+
+        // Buffer the vertices
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            (indices.len() * size_of::<T>()) as GLsizeiptr,
+            indices.as_ptr() as *const GLvoid,
+            gl::STATIC_DRAW,
+        );
+    }
+    return EBO {_id: ebo};
+}
+
+
+pub fn new_with_usage<T>(indices: &[T], usage: GLenum) -> EBO {
+    let mut ebo = 0;
+    unsafe {
+        // Generate the VBO
+        gl::GenBuffers(1, &mut ebo);
+        assert_ne!(ebo, 0);
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+
+        // Buffer the vertices
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            (indices.len() * size_of::<T>()) as GLsizeiptr,
+            indices.as_ptr() as *const GLvoid,
+            usage,
+        );
+    }
+    return EBO {_id: ebo};
 }
