8c7d55de866fd3e52a6b2c3ac482b62c4fae9fb4
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-02-23 13:18:46 -0300

    Removed unused import

--- src/utils/math/linalg.rs
+++ src/utils/math/linalg.rs
@@ -1,4 +1,4 @@
-use crate::utils::{math::trig, types};
+use crate::utils::types;
 
 pub fn mat6_mul3(m1: &[types::V6], m2: &[types::V3; 3]) -> Vec<types::V6> {
     let mut result = Vec::<types::V6>::new();
@@ -16,6 +16,40 @@ pub fn mat6_mul3(m1: &[types::V6], m2: &[types::V3; 3]) -> Vec<types::V6> {
     }
     return result;
 }
+
+// pub fn mat6_mul3(m1: &Vec<types::V6>, m2: &[types::V3; 3]) -> Vec<types::V6> {
+//     let mut result = Vec::<types::V6>::new();
+//     for i in 0..m1.len() {
+//         let vertex = m1[i];
+//         let mut temp_result: types::V6 = [0.0, 0.0, 0.0, vertex[3], vertex[4], vertex[5]];
+//         for j in 0..3 {
+//             let mut sum = 0.0;
+//             for k in 0..3 {
+//                 sum += vertex[k] * m2[k][j];
+//             }
+//             temp_result[j] = sum;
+//         }
+//         result.push(temp_result);
+//     }
+//     return result;
+// }
+
+// pub fn mat6_mul3<T: Index<usize>>(m1: &[T], m2: &[types::V3; 3]) -> Vec<T> {
+//     let mut result = Vec::<T>::new();
+//     for i in 0..m1.len() {
+//         let vertex = m1[i];
+//         let mut temp_result: T = [0.0, 0.0, 0.0, vertex[3], vertex[4], vertex[5]];
+//         for j in 0..3 {
+//             let mut sum = 0.0;
+//             for k in 0..3 {
+//                 sum += vertex[k] * m2[k][j];
+//             }
+//             temp_result[j] = sum;
+//         }
+//         result.push(temp_result);
+//     }
+//     return result;
+// }
 
 pub fn rot_mat_x(angle: f32) -> [types::V3; 3] {
     let ang_rad = angle.to_radians();
