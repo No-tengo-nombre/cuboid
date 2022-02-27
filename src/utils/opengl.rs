85db353f10582fd4f5f2c00df42fcdf4c63d795f
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-02-27 18:39:00 -0300

    Modified the message that shows when OpenGL is not loaded

--- src/utils/opengl.rs
+++ src/utils/opengl.rs
@@ -2,7 +2,8 @@ use crate::utils::init::GL_IS_LOADED;
 
 const GL_NOT_LOADED_MSG: &str = "
 ERROR: OpenGL is not currently loaded. Make sure to initalize it at the beginning of the program
-by running `init::init_gl(&window)`, where `window: glfw::Window`.
+by running init::init_gl(&window), where window is the instance of glfw::Window that you are
+using.
 ";
 
 /// This function checks that OpenGL is loaded, and should be called at the beginning of
