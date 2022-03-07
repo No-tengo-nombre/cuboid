902fff30d71069f914dda29fc320177087c1a28a
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-03-07 16:43:20 -0300

    Moved texture declaration outside of loop (speeds up quite a bit)

--- examples/texture_example/main.rs
+++ examples/texture_example/main.rs
@@ -11,7 +11,8 @@ use cuboid::components::{
     camera::PerspectiveCamera,
     material::Material,
     renderer3d::Renderer3D,
-    shape::Shape
+    shape::Shape,
+    texture::Texture2D,
 };
 use cuboid::core::shader::Shader;
 use cuboid::io::cam_controller::CameraController;
@@ -23,13 +24,14 @@ fn main() {
     let mut delta;
     let mut fps;
     let mut prev_time = 0.0;
-    let triangle_v: Vec<types::V6> = vec![
-        [-10.0, -10.0, 0.0, 1.0, 0.0, 0.0],
-        [10.0, -10.0, 0.0, 0.0, 1.0, 0.0],
-        [0.0, 10.0, 0.0, 0.0, 0.0, 1.0],
-    ];
+    let square_v: Vec<types::V8> = vec![
+        [-10.0, -10.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        [10.0, -10.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0],
+        [10.0, 10.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
+        [-10.0, 10.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0],
+        ];
 
-    let triangle_i: Vec<u32> = vec![0, 1, 2];
+    let square_i: Vec<u32> = vec![0, 1, 2, 3];
 
     let (mut window, events, mut glfw_instance) =
         init::init_glfw(1000, 1000, WINDOW_TITLE, glfw::WindowMode::Windowed);
@@ -41,15 +43,16 @@ fn main() {
         "examples/texture_example/resources/shaders/test.frag",
     );
     let material = Material::new(&shader);
+    let texture = Texture2D::new("examples/texture_example/resources/images/perroxd.png");
 
-    let triangle = Shape::new_with_usage(
-        &triangle_v,
-        &triangle_i,
+    let square = Shape::new_with_usage(
+        &square_v,
+        &square_i,
         &material,
-        &[0, 1],
+        &[0, 1, 2],
         gl::DYNAMIC_DRAW,
     );
-    renderer.add_item(&triangle);
+    renderer.add_item_with_mode(&square, gl::QUADS);
     let mut camera_pos = [0.0, 0.0, 20.0];
     let mut camera_dir = [0.0, 0.0, 1.0];
     let mut camera_up = [0.0, 1.0, 0.0];
@@ -142,9 +145,12 @@ fn main() {
         }
         camera.update(&camera_pos, &camera_dir, &camera_up);
 
+        
+        texture.bind();
         renderer.clear();
         renderer.render();
         window.swap_buffers();
+        texture.unbind();
         prev_time = time;
     }
 }
