820ba39e978d1cba6e4a54cce21b312a965a9881
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-03-14 18:58:28 -0300

    Added a transform structure

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,8 @@
 pub mod buffers;
 pub mod shader;
 pub mod traits;
+pub mod transform;
 
 pub use shader::Shader;
-pub use traits::Drawable;
+pub use traits::{Drawable, Transformable};
+pub use transform::Transform;
---
+++ src/core/transform.rs
@@ -0,0 +1,96 @@
+#[derive(Copy, Clone)]
+pub struct Transform {
+    _translate: TransformTranslate,
+    _rotate: TransformRotate,
+    _scale: TransformScale,  
+}
+
+#[derive(Copy, Clone)]
+struct TransformTranslate {
+    _x: f32,
+    _y: f32,
+    _z: f32,
+}
+
+#[derive(Copy, Clone)]
+struct TransformRotate {
+    _theta: f32,
+    _phi: f32,
+}
+
+#[derive(Copy, Clone)]
+struct TransformScale {
+    _x: f32,
+    _y: f32,
+    _z: f32,
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//|===============================| Transform implementation |==================================|//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+impl Transform {
+    pub fn new() -> Transform {
+        return Transform {
+            _translate: TransformTranslate::new(),
+            _rotate: TransformRotate::new(),
+            _scale: TransformScale::new(),
+        };
+    }
+
+    pub fn get_translate(&self) -> &TransformTranslate {
+        return &self._translate;
+    }
+
+    pub fn get_rotate(&self) -> &TransformRotate {
+        return &self._rotate;
+    }
+    pub fn get_scale(&self) -> &TransformScale {
+        return &self._scale;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//|================================| Transform components |=====================================|//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+impl TransformTranslate {
+    pub fn new() -> TransformTranslate {
+        return TransformTranslate::new_value(0.0, 0.0, 0.0);
+    }
+
+    pub fn new_value(x: f32, y: f32, z: f32) -> TransformTranslate {
+        return TransformTranslate {
+            _x: x,
+            _y: y,
+            _z: z,
+        };
+    }
+}
+
+impl TransformRotate {
+    pub fn new() -> TransformRotate {
+        return TransformRotate::new_value(0.0, 0.0);
+    }
+
+    pub fn new_value(theta: f32, phi: f32) -> TransformRotate {
+        return TransformRotate {
+            _theta: theta,
+            _phi: phi,
+        };
+    }
+}
+
+impl TransformScale {
+    pub fn new() -> TransformScale {
+        return TransformScale::new_value(1.0, 1.0, 1.0);
+    }
+
+    pub fn new_value(x: f32, y: f32, z: f32) -> TransformScale {
+        return TransformScale {
+            _x: x,
+            _y: y,
+            _z: z,
+        };
+    }
+}
