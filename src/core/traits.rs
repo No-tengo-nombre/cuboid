e44210d90104c939be999d26f4f967b7807d925e
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-03-14 18:56:21 -0300

    Added a new trait for transformable objects

--- src/core/traits.rs
+++ src/core/traits.rs
@@ -1,6 +1,11 @@
 use gl::types::*;
+use crate::core::Transform;
 
 /// An object that defines how it should be drawn by the renderer
 pub trait Drawable {
     fn get_drawn(&self, mode: GLenum);
+}
+
+pub trait Transformable {
+    fn apply_transform(&self, transform: &Transform) -> dyn Transformable;
 }
