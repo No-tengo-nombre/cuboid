7fd79c9f936ec80f35797380e6192afccc71945b
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-03 23:45:05 -0400

    Compressed the image loading code

--- src/utils/images.rs
+++ src/utils/images.rs
@@ -1,23 +1,8 @@
 use image::io::Reader;
+use std::path::Path;
 
 pub fn load(path: &str) -> (image::DynamicImage, i32, i32) {
-    let img = match Reader::open(&std::path::Path::new(path)) {
-        Ok(value) => value,
-        Err(error) => {
-            println!("Error reading image {}", path);
-            println!("{:?}", error);
-            std::process::exit(0);
-        }
-    };
-
-    let img_data = match img.decode() {
-        Ok(value) => value,
-        Err(error) => {
-            println!("Error reading image {}", path);
-            println!("{:?}", error);
-            std::process::exit(0);
-        }
-    };
+    let img_data = Reader::open(&Path::new(path)).unwrap().decode().unwrap();
 
     let width = img_data.width().try_into().unwrap();
     let height = img_data.height().try_into().unwrap();
