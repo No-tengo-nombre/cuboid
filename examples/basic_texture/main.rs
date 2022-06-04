0dd8b674cd96527cdc1542583cb1bee59fa1f1cf
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-03 22:30:33 -0400

    Style changes and made the example consistent with the API changes

--- examples/basic_texture/main.rs
+++ examples/basic_texture/main.rs
@@ -7,11 +7,11 @@ use glfw::Context;
 use controller::Controller;
 use cuboid::components::{
     Camera,
-    OrthoCamera,
     PerspectiveCamera,
     Material,
     Renderer3D,
     Shape,
+    Texture2D,
 };
 use cuboid::Shader;
 use cuboid::io::CameraController;
@@ -32,49 +32,54 @@ fn main() {
     ];
     let axes_i: Vec<u32> = vec![0, 1, 0, 2, 0, 3];
 
-    let square_v: Vec<types::V8> = vec![
-        [-10.0, -10.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
-        [10.0, -10.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0],
-        [10.0, 10.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
-        [-10.0, 10.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0],
+    // let square_v: Vec<types::V8> = vec![
+    //     [-4.0, -10.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+    //     [4.0, -10.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0],
+    //     [6.0, 10.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
+    //     [-6.0, 10.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0],
+    // ];
+    let square_v: Vec<types::V6> = vec![
+        [-4.0, -10.0, 0.0, 1.0, 0.0, 0.0],
+        [4.0, -10.0, 0.0, 0.0, 1.0, 0.0],
+        [6.0, 10.0, 0.0, 0.0, 0.0, 1.0],
+        [-6.0, 10.0, 0.0, 1.0, 1.0, 1.0],
     ];
 
     let square_i: Vec<u32> = vec![0, 1, 2, 3];
 
     let (mut window, events, mut glfw_instance) = cuboid::Window::new()
-        .width(1000)
-        .height(1000)
+        .dimensions(1000, 1000)
         .title(WINDOW_TITLE)
         .windowed()
         .build();
     let mut renderer = Renderer3D::new();
     renderer.set_clear_color(0.0, 0.0, 0.0, 1.0);
-    let axes_shader = Shader::new(
-        "examples/basic_texture/resources/shaders/test.vert",
-        "examples/basic_texture/resources/shaders/line.frag",
-    );
-    let cube_shader = Shader::new(
-        "examples/basic_texture/resources/shaders/test.vert",
-        "examples/basic_texture/resources/shaders/test.frag",
-    );
-    let axes_material = Material::new(&axes_shader);
-    let cube_material = Material::new(&cube_shader);
 
-    let axes = Shape::new_with_usage(
-        &axes_v,
-        &axes_i,
-        &axes_material,
-        &[0, 1],
-        gl::STATIC_DRAW,
-    );
-    let mut square = Shape::new_with_usage(
-        &square_v,
-        &square_i,
-        &cube_material,
-        &[0, 1, 2],
-        gl::DYNAMIC_DRAW,
-    );
-    // square.set_texture_path("examples/texture_example/resources/images/perroxd.png");
+    let axes_shader = Shader::new()
+        .vertex("examples/basic_texture/resources/shaders/test.vert")
+        .fragment("examples/basic_texture/resources/shaders/line.frag");
+    let cube_shader = Shader::new()
+        .vertex("examples/basic_texture/resources/shaders/test.vert")
+        .fragment("examples/basic_texture/resources/shaders/test.frag");
+    let axes_material = Material::new().shader(&axes_shader);
+    let cube_material = Material::new().shader(&cube_shader);
+
+    let axes = Shape::new()
+        .vertices(&axes_v)
+        .indices(&axes_i)
+        .material(&axes_material)
+        .layouts(&[0, 1])
+        .build();
+    let mut square = Shape::new()
+        .vertices(&square_v)
+        .indices(&square_i)
+        .material(&cube_material)
+        // .layouts(&[0, 1, 2])
+        .layouts(&[0, 1])
+        .usage(gl::DYNAMIC_DRAW)
+        .vert_sizes(&[3, 3, 2])
+        .texture(&Texture2D::new().from_path("examples/texture_example/resources/images/perroxd.png"))
+        .build();
 
     // renderer.add_item_with_mode(&axes, gl::LINE);
     renderer.add_item_with_mode(&square, gl::QUADS);
@@ -82,7 +87,6 @@ fn main() {
     let mut camera_dir = [0.0, 0.0, 1.0];
     let mut camera_up = [0.0, 1.0, 0.0];
     let mut camera_right = linalg::normalize_v3(&linalg::cross_v3(&camera_up, &camera_dir));
-    // let mut camera_right;
 
     let mut camera = PerspectiveCamera::new(
         &camera_pos,
@@ -111,8 +115,6 @@ fn main() {
         delta = time - prev_time;
         fps = 1.0 / delta;
         // println!("FPS : {}", fps);
-
-        // camera_right = linalg::normalize_v3(&linalg::cross_v3(&camera_up, &camera_dir));
 
         if wireframe != controller.wireframe {
             if controller.wireframe {
@@ -150,39 +152,26 @@ fn main() {
         // Camera rotation control
         if controller.up_pressed {
             camera_dir = linalg::mat3_mul_v3(
-                &linalg::rot_mat3(&camera.right, cam_rot_speed),
+                &linalg::rot_mat3(&camera._right, cam_rot_speed),
                 &camera_dir,
             );
             camera_up =
-                linalg::mat3_mul_v3(&linalg::rot_mat3(&camera.right, cam_rot_speed), &camera_up);
+                linalg::mat3_mul_v3(&linalg::rot_mat3(&camera._right, cam_rot_speed), &camera_up);
         }
         if controller.down_pressed {
             camera_dir =
-                linalg::mat3_mul_v3(&linalg::rot_mat3(&camera.right, -cam_rot_speed), &camera_dir);
+                linalg::mat3_mul_v3(&linalg::rot_mat3(&camera._right, -cam_rot_speed), &camera_dir);
             camera_up =
-                linalg::mat3_mul_v3(&linalg::rot_mat3(&camera.right, -cam_rot_speed), &camera_up);
+                linalg::mat3_mul_v3(&linalg::rot_mat3(&camera._right, -cam_rot_speed), &camera_up);
         }
         if controller.left_pressed {
-            camera_dir = linalg::mat3_mul_v3(&linalg::rot_mat3(&camera.up, cam_rot_speed), &camera_dir);
+            camera_dir = linalg::mat3_mul_v3(&linalg::rot_mat3(&camera._up, cam_rot_speed), &camera_dir);
         }
         if controller.right_pressed {
-            camera_dir = linalg::mat3_mul_v3(&linalg::rot_mat3(&camera.up, -cam_rot_speed), &camera_dir);
+            camera_dir = linalg::mat3_mul_v3(&linalg::rot_mat3(&camera._up, -cam_rot_speed), &camera_dir);
         }
-        camera_right = camera.right;
-        // camera.update(&camera_pos, &camera_dir, &camera_right);
+        camera_right = camera._right;
         camera.update(&camera_pos, &camera_dir, &camera_up);
-        println!(
-            "DIR ({}, {}, {})   UP ({}, {}, {})   RIGHT({}, {}, {})",
-            camera_dir[0],
-            camera_dir[1],
-            camera_dir[2],
-            camera_up[0],
-            camera_up[1],
-            camera_up[2],
-            camera_right[0],
-            camera_right[1],
-            camera_right[2],
-        );
 
         renderer.clear();
         renderer.render();
